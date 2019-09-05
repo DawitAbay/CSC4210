908463df0cc4606f69700ab0891df921e32fc794
DawitAbay <46979460+DawitAbay@users.noreply.github.com>
2019-09-05 14:54:59 -0400

    Add files via upload

---
+++ robot.py
@@ -0,0 +1,17 @@
+ from gpiozero import Motor
+ from time import sleep
+
+ mot1 = Motor(4.5)
+ mot1.forward(0.5)
+ time.sleep(3)
+ mot1.stop()
+
+
+ # Move the robot forwards
+     
+
+ # Move the robot backwards
+
+ # Move the robot left
+
+ # Move the robot right
